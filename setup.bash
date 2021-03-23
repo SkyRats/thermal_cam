a8da1d700d7ed0e5f32c5b75f08900fb1b3fcfce
Felipe Cardenas Lima Namour <felipe.cln@usp.br>
2021-03-23 14:57:17 -0300

    First

---
+++ setup.bash
@@ -0,0 +1,4 @@
+sudo apt-get install -y python-smbus python-dev i2c-tools
+sudo adduser pi i2c
+sudo pip3 install wheel flask
+sudo pip3 install adafruit-blinka #https://learn.adafruit.com/circuitpython-libaries-linux-odroid-c2/initial-setup
